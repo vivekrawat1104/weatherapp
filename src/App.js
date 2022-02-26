ac3efb37309a31d53775a6577b5a85df53edbc43
Vivek <Vivekrawat1104@gmail.com>
2022-02-27 00:47:57 +0530

    Added website title and logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Temp from './components/weather/temp.js';
-
-
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <>
+      {/* Set Title of the Website: */}
+      <Helmet>
+            <title>Weather App</title>
+            <meta name="description" content="Weather Application (Live)" />
+      </Helmet>
       {/* 1. Call the Temp-Component: */}
       <Temp/>
                
